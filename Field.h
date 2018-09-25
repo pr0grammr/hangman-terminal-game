a22fa7fe2fba3546be832bba9f1b18f956151234
Fabian Schilf <f.schilf94@gmail.com>
2018-09-25 19:37:53 +0200

    created empty classes for Game and Field

---
+++ Field.h
@@ -0,0 +1,10 @@
+#ifndef HANGMAN_TERMINAL_GAME_FIELD_H
+#define HANGMAN_TERMINAL_GAME_FIELD_H
+
+
+class Field {
+
+};
+
+
+#endif
---
+++ Game.h
@@ -0,0 +1,10 @@
+#ifndef HANGMAN_TERMINAL_GAME_GAME_H
+#define HANGMAN_TERMINAL_GAME_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif
