1e8b96f68ce0c02b464d9c13f379dedd5ba6d964
Fabian Schilf <f.schilf94@gmail.com>
2018-09-27 23:47:39 +0200

    add compiled file

---
+++ build/CMakeFiles/hangman.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hangman.dir/main.cpp.o"
+  "CMakeFiles/hangman.dir/Game.cpp.o"
+  "hangman.pdb"
+  "hangman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
